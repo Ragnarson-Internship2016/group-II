7ffa0b73c2414f53cdfa906af564e77d1831827a
Wojciech Pospieszyski <pospieszynski.wojciech@gmail.com>
2016-07-20 12:28:14 +0200

    spec refactor and removed unnecessary autofocus from some input fields

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject(:user) { User.create(name: "Wojtek", surname: "Pośpiech", email: "a@a.pl", password: "topsecret") }
+
   context "With a proper validations setup user" do
-    let(:user) { User.create(name: "Wojtek", surname: "Pośpiech", email: "a@a.pl", password: "topsecret") }
+    it "should be valid with all params provided" do
+      expect(user).to be_valid
+    end
+  end
+
+  context "With improper validations setup user" do
     it "should not be valid without a name" do
       user.name = nil
       expect(user).not_to be_valid
     end
+
     it "should not be valid without a surname" do
       user.surname = nil
       expect(user).not_to be_valid
-    end
-    it "should be valid with all params provided" do
-      expect(user).to be_valid
     end
   end
 end
