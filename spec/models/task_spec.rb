489b79e276e80c2d3f9865a63192c12309b7e932
karolcircle <serwisnet1@gmail.com>
2016-07-20 16:41:38 +0200

    Changes in specs

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,36 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  let(:title) { "Short text" }
-  let(:description) { "Long text" }
-  let(:due_date) { "11-11-1999" } 
+  subject(:task) { Task.create(title: "Short text", description: "Long text", due_date: "11-11-1999") }
   
-  context "when valid params" do
-    it "saves task with all params and without done status" do
-      task = Task.new(title: title, description: description, due_date: due_date)
-      expect(task.save).to eq(true)
-    end
-
-    it "saves task with all params and true done status" do
-      task = Task.new(title: title, description: description, due_date: due_date, done: true)
-      expect(task.save).to eq(true)
-    end
+  context "With a proper task validations" do
+    it "it is valid with all params provided" do
+      expect(task).to be_valid
+    end    
   end
 
-  context "when invalid params" do
-    it "does not save task without title" do
-      task = Task.new(title: nil, description: description, due_date: due_date)
-      expect(task.save).to eq(false)
+  context "With improper task validations" do
+    it "is not valid without a title" do
+      task.title = nil
+      expect(task).not_to be_valid
     end
 
-    it "does not save task without description" do
-      task = Task.new(title: title, description: nil, due_date: due_date)
-      expect(task.save).to eq(false)
+    it "is not valid without a description" do
+      task.description = nil
+      expect(task).not_to be_valid
     end
 
-    it "does not save task without date" do
-      task = Task.new(title: title, description: description, due_date: nil)
-      expect(task.save).to eq(false)
+    it "is not valid without a due_date" do
+      task.due_date = nil
+      expect(task).not_to be_valid
     end
   end
 end
