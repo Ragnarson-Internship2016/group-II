27b8f53f4f98c637792389792044bb830892ef45
Marta Sajdak <crappylime@op.pl>
2016-07-20 14:06:49 +0300

    change language to english in examples for spec
    - #12

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,20 +3,20 @@ require 'rails_helper'
 RSpec.describe Project, type: :model do
   context "invalid params" do
     it "does not save project without title" do
-      project = Project.new(date: "20-07-2016", description: "o niczym")
+      project = Project.new(date: "20-07-2016", description: "about nothing")
       expect(project.save).to eq(false)
     end
 
     it "does not save project with too long title" do
-      title = "title" * 21
-      project = Project.new(title: title, date: "20-07-2016", description: "o niczym")
+      title = "nothing" * 21
+      project = Project.new(title: title, date: "20-07-2016", description: "about nothing")
       expect(project.save).to eq(false)
     end
   end
 
   context "valid params" do
     it "saves projects when all params are given" do
-      project = Project.new(title: "tytuł", date: "20-07-2016", description: "o niczym")
+      project = Project.new(title: "nothing", date: "20-07-2016", description: "about nothing")
       expect(project.save).to eq(true)
     end
   end
