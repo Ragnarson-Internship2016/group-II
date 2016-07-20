c6cf58dd5a7def66ea01007a806f660237cf23f6
Wojciech Pospieszyski <pospieszynski.wojciech@gmail.com>
2016-07-20 12:58:48 +0200

    removed should from spec exampe desc.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,18 +4,18 @@ RSpec.describe User, type: :model do
   subject(:user) { User.create(name: "Wojtek", surname: "Pośpiech", email: "a@a.pl", password: "topsecret") }
 
   context "With a proper validations setup user" do
-    it "should be valid with all params provided" do
+    it "is valid with all params provided" do
       expect(user).to be_valid
     end
   end
 
   context "With improper validations setup user" do
-    it "should not be valid without a name" do
+    it "is not valid without a name" do
       user.name = nil
       expect(user).not_to be_valid
     end
 
-    it "should not be valid without a surname" do
+    it "is not valid without a surname" do
       user.surname = nil
       expect(user).not_to be_valid
     end
