0d533a5b566688920cea630a9e5ed2b1fa28a1b5
Marta Sajdak <crappylime@op.pl>
2016-07-20 17:17:53 +0300

    improve context title in project_spec
    - #12

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  context "invalid params" do
+  context "with invalid params" do
     it "does not save project without title" do
       project = Project.new(date: "20-07-2016", description: "about nothing")
       expect(project.save).to eq(false)
     end
   end
 
-  context "valid params" do
+  context "with valid params" do
     it "saves projects when all params are given" do
       project = Project.new(title: "nothing", date: "20-07-2016", description: "about nothing")
       expect(project.save).to eq(true)
