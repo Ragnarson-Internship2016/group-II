2d555062beda951e7f4c3bde55b35b43c0f762d0
Wojciech Pospieszyski <pospieszynski.wojciech@gmail.com>
2016-07-20 03:11:02 +0200

    removed unnecessary files, added validations and spec for user model

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
-
   private
-
   def sign_up_params
     params.require(:user).permit(:name, :surname, :email, :password, :password_confirmation)
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "With a proper validations setup user" do
+    before :each do
+      @user = User.create(name: "Wojtek", surname: "Pośpiech")
+    end
+    it "should not be valid without a name" do
+      @user.name = nil
+      @user.should_not be_valid
+    end
+    it "should not be valid without a surname" do
+      @user.surname = nil
+      @user.should_not be_valid
+    end
+  end
 end
