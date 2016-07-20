5cc9d166a1c985317ddd5e9ae8b1d2c2c48df6a7
pospieszynski <pospieszynski.wojciech@gmail.com>
2016-07-20 13:08:58 +0200

    User model with devise (#46)
    * Basic devise setup
    * Added name and surname to users table
    * Added input fields to sign up view and overwritten registration controller for additional fields
    * removed unnecessary files, added validations and spec for user model
    * removed views generated by devise for customization that will not be customized
    * Specs optimized, adding surname and name to edit user form
    * added missing empty line that was accidenatly removed(database.yaml)
    * spec refactor and removed unnecessary autofocus from some input fields
    * removed should from spec exampe desc.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject(:user) { User.create(name: "Wojtek", surname: "Pośpiech", email: "a@a.pl", password: "topsecret") }
+
+  context "With a proper validations setup user" do
+    it "is valid with all params provided" do
+      expect(user).to be_valid
+    end
+  end
+
+  context "With improper validations setup user" do
+    it "is not valid without a name" do
+      user.name = nil
+      expect(user).not_to be_valid
+    end
+
+    it "is not valid without a surname" do
+      user.surname = nil
+      expect(user).not_to be_valid
+    end
+  end
+end
