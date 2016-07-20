eead701e6b35a0d8621f88828189b20ef8cb39f5
Marta Sajdak <crappylime@op.pl>
2016-07-20 11:20:38 +0300

    add validations and tests for Project model
    - #12

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ApplicationRecord
+  validates :title, :date, :description, presence: { message: "must be given please" }
+  validates :title, length: { maximum: 100, too_long: "%{count} characters is the maximum allowed" }
+  validates :description, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed" }
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "invalid params" do
+    it "does not save project without title" do
+      project = Project.new(id:1, date: "20-07-2016", description: "o niczym")
+      expect(project.save).to eq(false)
+    end
+    it "does not save project with too long title" do
+      project = Project.new(id:2, title: "projekt o bardzo długim, długim, długim, długim, długim, długim, długim, długim za długim tytule o kilka znaków", date: "20-07-2016", description: "o niczym")
+      expect(project.save).to eq(false)
+    end
+  end
+
+  context "valid params" do
+    it "saves projects when all params are given" do
+      project = Project.new(id:1, title: "tytuł", date: "20-07-2016", description: "o niczym")
+      expect(project.save).to eq(true)
+    end
+  end
 end
