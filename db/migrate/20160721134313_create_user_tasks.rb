6d7f3f5e6ded738a081e5e34aff8672dd1b83e5c
tommy <tommpoland@gmail.com>
2016-07-21 16:17:46 +0200

    Migrations changed - index added

--- db/migrate/20160721134313_create_user_tasks.rb
+++ db/migrate/20160721134313_create_user_tasks.rb
@@ -1,10 +1,11 @@
 class CreateUserTasks < ActiveRecord::Migration[5.0]
   def change
     create_table :user_tasks do |t|
-      t.references :user, foreign_key: true
-      t.references :task, foreign_key: true
+      t.references :user, index: true, foreign_key: true
+      t.references :task, index: true, foreign_key: true
 
       t.timestamps
     end
+    add_index :user_tasks, [:user_id, :task_id], unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20160721134313) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["task_id"], name: "index_user_tasks_on_task_id", using: :btree
+    t.index ["user_id", "task_id"], name: "index_user_tasks_on_user_id_and_task_id", unique: true, using: :btree
     t.index ["user_id"], name: "index_user_tasks_on_user_id", using: :btree
   end
 
