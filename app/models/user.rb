f8a0321fc93c4443e09065a7248e3d99e6d7797a
tommy <tommpoland@gmail.com>
2016-07-21 15:51:23 +0200

    Added many to many association for user and task + migrations
    To achieve this I created model UserTask

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,7 @@ class User < ApplicationRecord
   has_many :managed_projects, class_name: "Project"
   has_many :user_projects
   has_many :contributed_projects, through: :user_projects, source: :project
+
+  has_many :user_tasks
+  has_many :assigned_tasks, through: :user_tasks, source: :task
 end
---
+++ spec/factories/user_tasks.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user_task do
+    user nil
+    task nil
+  end
+end
