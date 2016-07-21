4f450b66fcf3471714f6e765167e6d8f48c660bc
Wojciech Pospieszyski <pospieszynski.wojciech@gmail.com>
2016-07-21 11:19:03 +0200

    added foreign key constaraint in user projects table

--- db/migrate/20160720194304_add_user_id_to_projects.rb
+++ db/migrate/20160720194304_add_user_id_to_projects.rb
@@ -1,5 +1,5 @@
 class AddUserIdToProjects < ActiveRecord::Migration[5.0]
   def change
-    add_column :projects, :user_id, :integer
+    add_column :projects, :user_id, :integer, foreign_key: true
   end
 end
